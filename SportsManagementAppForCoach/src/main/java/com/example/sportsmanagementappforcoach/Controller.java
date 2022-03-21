869deebe470e555921c613efdd6c58f7d102de41
galibmahmudjim <galibmahmudjim@gmail.com>
2022-03-21 11:01:24 +0600

    login(almost)

--- SportsManagementAppForCoach/src/main/java/com/example/sportsmanagementappforcoach/Controller.java
+++ SportsManagementAppForCoach/src/main/java/com/example/sportsmanagementappforcoach/Controller.java
@@ -1,39 +1,40 @@
 package com.example.sportsmanagementappforcoach;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    private TextField CoachName;
-
-    @FXML
-    private TextField Password;
-
+public class Controller implements Initializable
+{
     @FXML
     private Button loginButton;
-
     @FXML
-    String getCoachName() {
-        return CoachName.getText();
-    }
-
+    private Button SignUpButton;
     @FXML
-    String getPassword() {
-        return Password.getText();
-    }
-
+    private TextField TextFieldUsername;
     @FXML
-    private Label wrongLoginInformation;
+    private TextField TextFieldPassword;
 
-    @FXML
-    void onLoginClick(ActionEvent event) {
-        String UserName = getCoachName();
-        String UserPassword = getPassword();
-        wrongLoginInformation.setText("Your Password of User Name is incorrect");
+    @Override
+   public void initialize(URL url, ResourceBundle resourceBundle) {
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.logInUser(event, TextFieldUsername.getText(), TextFieldPassword.getText());
+            }
+        });
+        SignUpButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                  DBUtils.change_scene(event,"signup.fxml","SignUp",null);
+            }
+        });
     }
 }
--- SportsManagementAppForCoach/src/main/java/com/example/sportsmanagementappforcoach/Main.java
+++ SportsManagementAppForCoach/src/main/java/com/example/sportsmanagementappforcoach/Main.java
@@ -1,7 +1,9 @@
 package com.example.sportsmanagementappforcoach;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,14 +12,14 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Sports Management App for Coach");
-        stage.setScene(scene);
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        stage.setTitle("Welcome");
+        stage.setScene(new Scene(root,600,500));
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
+
 }
