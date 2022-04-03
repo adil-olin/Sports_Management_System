9b643f4c8f4e4788700b111ca92b1f71d7eaf8d8
galibmahmudjim <galibmahmudjim@gmail.com>
2022-04-04 04:07:00 +0600

    VBox and ButtonList

--- SportsManagementAppForCoach/src/main/java/com/example/sportsmanagementappforcoach/HomePage.java
+++ SportsManagementAppForCoach/src/main/java/com/example/sportsmanagementappforcoach/HomePage.java
@@ -1,18 +1,27 @@
 package com.example.sportsmanagementappforcoach;
 
 import COACH.Coach;
+import DBUtil.DBConnection;
 import DBUtil.DBResources;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class HomePage {
+public class HomePage implements Initializable{
 
     @FXML
     private Button HomePageAddTeamButton;
@@ -23,7 +32,10 @@ public class HomePage {
     @FXML
     private Label HomePageUserNameLabel;
 
-    ArrayList<String>HomePageTeamNames;
+    ArrayList<String>HomePageTeamNames=new ArrayList<>();
+
+    @FXML
+    private VBox HomePageAddTeamButtonVBox;
 
     @FXML
     void OnHomePageAddTeamButtonClick(ActionEvent event) throws SQLException, IOException {
@@ -37,17 +49,48 @@ public class HomePage {
         sceneController.SwitchToFirstPage(event);
     }
 
-
+    private ArrayList<Button> HomePageTeamNameButtonList = new ArrayList<>();
     private Coach coach;
-
     void setUserNameLabel(String mailid) throws SQLException {
         DBResources dbResources = new DBResources();
         HomePageTeamNames = dbResources.getTeamLists(mailid);
         coach = dbResources.getCoachData(mailid);
         this.HomePageUserNameLabel.setText(coach.getName());
+
+        int i = 3;
+        for(i = 0; i < HomePageTeamNames.size(); i ++)
+        {
+            Button temp = new Button();
+            temp.setText(HomePageTeamNames.get(i));
+            HomePageTeamNameButtonList.add(temp);
+        }
+        HomePageAddTeamButtonVBox.getChildren().addAll(HomePageTeamNameButtonList);
+
+
+
     }
+    @FXML
+    void OnHomePageVboxMouseClick(MouseEvent event) {
+        for(int i = 0; i < HomePageAddTeamButtonVBox.getChildren().size();i++)
+        {
+            Button button = (Button) HomePageAddTeamButtonVBox.getChildren().get(i);
+
+        }
+    }
+
     String getUserNameLabel()
     {
         return HomePageUserNameLabel.getText();
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        DBResources dbResources = new DBResources();
+        Coach coachname = new Coach();
+        try {
+            HomePageTeamNames = dbResources.getTeamLists(coachname.getEmailId());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
