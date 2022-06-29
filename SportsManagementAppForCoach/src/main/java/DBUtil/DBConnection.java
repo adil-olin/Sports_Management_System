0cde050db71293e5dd9b5fc92fce69181d6cc199
galibmahmudjim <galibmahmudjim@gmail.com>
2022-06-29 13:07:14 +0600

    jar

--- SportsManagementAppForCoach/src/main/java/DBUtil/DBConnection.java
+++ SportsManagementAppForCoach/src/main/java/DBUtil/DBConnection.java
@@ -1,5 +1,6 @@
 package DBUtil;
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,16 +10,22 @@ public class DBConnection {
     private static final String USERNAME = "dbuser";
     private static final String PASSWORD = "dbpassword";
     private static final String conn = "jdbc:mysql://localhost/login";
-    private static final String sqconn = "jdbc:sqlite:Coach.sqlite";
-    //private static final String sqconn = "jdbc:sqlite:/Users/galibmahmudjim/Documents/GitHub/Sports_Management_System/SportsManagementAppForCoach/Coach.sqlite";
-    public static Connection getConnection()
-    {
+    //private static final String sqconn = "jdbc:sqlite:Coach.sqlite";
+    private static String sqconn;
+
+    public static Connection getConnection()  {
+
         try {
+            String sqconn = "jdbc:sqlite:" + new java.io.File(".").getCanonicalPath() + "/Coach.sqlite";
+            System.out.println(sqconn);
+
             Class.forName("org.sqlite.JDBC");
             return DriverManager.getConnection(sqconn);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
